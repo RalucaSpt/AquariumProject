4c4ef84273c66d55acb940a8eae13c32257f682e
Antonia Stanila <antonimaria.stanila@gmail.com>
2024-05-15 15:04:55 +0300

    modified Shader.h and implemented constructor and destructor for Shader

--- Aquarium/Aquarium/Shader.cpp
+++ Aquarium/Aquarium/Shader.cpp
@@ -1,1 +1,11 @@
 #include "Shader.h"
+
+Shader::Shader(const char* vertexPath, const char* fragmentPath)
+{
+	Init(vertexPath, fragmentPath);
+}
+
+Shader::~Shader()
+{
+	glDeleteProgram(ID);
+}
--- Aquarium/Aquarium/Shader.h
+++ Aquarium/Aquarium/Shader.h
@@ -1,149 +1,46 @@
 #pragma once
-#include <fstream>
-#include <fwd.hpp>
-#include <iostream>
-#include <ostream>
-#include <sstream>
-#include <vec3.hpp>
 #include <GL/glew.h>
-#include <gtc/type_ptr.hpp>
+#include <GLM.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
 
 class Shader
 {
 public:
     // constructor generates the shaderStencilTesting on the fly
     // ------------------------------------------------------------------------
-    Shader() {}
-
-    Shader(const char* vertexPath, const char* fragmentPath)
-    {
-        Init(vertexPath, fragmentPath);
-    }
-
-    ~Shader()
-    {
-        glDeleteProgram(ID);
-    }
+    Shader(const char* vertexPath, const char* fragmentPath);
+    ~Shader();
 
     // activate the shaderStencilTesting
     // ------------------------------------------------------------------------
-    void Use() const
-    {
-        glUseProgram(ID);
-    }
+    void Use() const;
 
-    unsigned int GetID() const { return ID; }
-    void SetVec4(const char* str, glm::vec4 vec)
-    {
-    	glUniform4fv(glGetUniformLocation(ID, str), 1, glm::value_ptr(vec));
-    }
+    unsigned int GetID() const;
+    
+   
+    void SetInt(const std::string& name, int value) const;
+    void SetFloat(const std::string& name, float value) const;
+    void SetVec2(const std::string& name, const glm::vec2& value) const;
+    void SetVec3(const std::string& name, const glm::vec3& value) const;
+    void SetVec3(const std::string& name, float x, float y, float z) const;
+    void SetMat4(const std::string& name, const glm::mat4& mat) const;
+    void SetVec4(const std::string& name, const glm::vec4& value) const;
 
-    // MVP
+
+
+private:
+    void Init(const char* vertexPath, const char* fragmentPath);
+    void CheckCompileErrors(unsigned int shaderStencilTesting, std::string type);
+
+private:
     unsigned int loc_model_matrix;
     unsigned int loc_view_matrix;
     unsigned int loc_projection_matrix;
 
-    // utility uniform functions
-    void SetInt(const std::string& name, int value) const
-    {
-        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
-    }
-    void SetFloat(const std::string& name, float value) const
-    {
-        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
-    }
-    void SetVec3(const std::string& name, const glm::vec3& value) const
-    {
-        glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
-    }
-    void SetVec3(const std::string& name, float x, float y, float z) const
-    {
-        glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
-    }
-    void SetMat4(const std::string& name, const glm::mat4& mat) const
-    {
-        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
-    }
-
-
-private:
-    void Init(const char* vertexPath, const char* fragmentPath)
-    {
-        // 1. retrieve the vertex/fragment source code from filePath
-        std::string vertexCode;
-        std::string fragmentCode;
-        std::ifstream vShaderFile;
-        std::ifstream fShaderFile;
-        // ensure ifstream objects can throw exceptions:
-        vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-        fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-        try {
-            // open files
-            vShaderFile.open(vertexPath);
-            fShaderFile.open(fragmentPath);
-            std::stringstream vShaderStream, fShaderStream;
-            // read file's buffer contents into streams
-            vShaderStream << vShaderFile.rdbuf();
-            fShaderStream << fShaderFile.rdbuf();
-            // close file handlers
-            vShaderFile.close();
-            fShaderFile.close();
-            // convert stream into string
-            vertexCode = vShaderStream.str();
-            fragmentCode = fShaderStream.str();
-        }
-        catch (std::ifstream::failure e) {
-            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
-        }
-        const char* vShaderCode = vertexCode.c_str();
-        const char* fShaderCode = fragmentCode.c_str();
-
-        // 2. compile shaders
-        unsigned int vertex, fragment;
-        // vertex shaderStencilTesting
-        vertex = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertex, 1, &vShaderCode, NULL);
-        glCompileShader(vertex);
-        CheckCompileErrors(vertex, "VERTEX");
-        // fragment Shader
-        fragment = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fragment, 1, &fShaderCode, NULL);
-        glCompileShader(fragment);
-        CheckCompileErrors(fragment, "FRAGMENT");
-        // shaderStencilTesting Program
-        ID = glCreateProgram();
-        glAttachShader(ID, vertex);
-        glAttachShader(ID, fragment);
-        glLinkProgram(ID);
-        CheckCompileErrors(ID, "PROGRAM");
-
-        // 3. delete the shaders as they're linked into our program now and no longer necessery
-        glDeleteShader(vertex);
-        glDeleteShader(fragment);
-    }
-
-    // utility function for checking shaderStencilTesting compilation/linking errors.
-    // ------------------------------------------------------------------------
-    void CheckCompileErrors(unsigned int shaderStencilTesting, std::string type)
-    {
-        GLint success;
-        GLchar infoLog[1024];
-        if (type != "PROGRAM") {
-            glGetShaderiv(shaderStencilTesting, GL_COMPILE_STATUS, &success);
-            if (!success) {
-                glGetShaderInfoLog(shaderStencilTesting, 1024, NULL, infoLog);
-                std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
-            }
-        }
-        else {
-            glGetProgramiv(shaderStencilTesting, GL_LINK_STATUS, &success);
-            if (!success) {
-                glGetProgramInfoLog(shaderStencilTesting, 1024, NULL, infoLog);
-                std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
-            }
-        }
-    }
-public:
     unsigned int ID;
 };
 
